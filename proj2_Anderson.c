f5668e67a856f9f2652d0f67564649fe005c48eb
ga17 <ga17@my.fsu.edu>
2020-02-27 16:47:42 -0500

    Added files

--- proj2_Anderson.c
+++ proj2_Anderson.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+	int instruction, immed;
+	char type;
+	int rs, rt, rd, branchTarget;
+}state;
+
 int get_byte(int src, int pos);
-void print_byte(int src);
+void print_byte(int src, int num);
 
 void print(int cycle, int pc, int *dataMem, int *regFile);
 
+void init_state(state* st);
+
+int get_op(int instruction);
+
 int main(){
 
-	int cycle = 0, pc = 0;
-	int dataMem[32], regFile[32];
+	
+	int instruction[100], dataMem[32], regFile[32];
+	state pipe1, pipe2, pipe3, pipe4;
+
+	for(int i = 0; i < 100; ++i){ instruction[i] = 0; }
 	for(int i = 0; i < 32; ++i){ dataMem[i] = i; regFile[i] = i; }
 
 	char input[1491]; // max word, instr char lines
-	int instruction;
 
+	int i = 0;
 	while(fgets(input, sizeof(input), stdin) != NULL){
-		instruction = atoi(input);
-		//printf("Instruction: %d\n", instruction);
-		print_byte(instruction);
+		instruction[i++] = atoi(input);
+	}
+	instruction[i] = -1;
+
+	for(int i = 0; i < 100; ++i){
+		if(instruction[i] == -1)
+			break;
+		print_byte(instruction[i]);
+		printf("\top code: ");
+		print_byte(get_op(instruction[i]));
 		printf("\n");
 	}
 
@@ -29,8 +49,8 @@ int main(){
 
 int get_byte(int src, int pos){	return (src >> (4*(pos-1))) & 15; }
 
-void print_byte(int src){
-	for(int i = 31; i >= 0; --i){
+void print_byte(int src, int num){
+	for(int i = 31 - (31 - (num-1)); i >= 0; --i){
 		printf("%d", (src >> i) & 1);
 		if(i%4==0 && i != 31 && i != 0)
 			printf(" ");
@@ -83,3 +103,15 @@ void print(int cycle, int pc, int *dataMem, int *regFile){
 
 	printf("********************\n");
 }
+
+void init_state(state* st){
+	st->instruction = st->immed = 0;
+	st->type = 0;
+	st->rs = st->rt = st->rd = st->branchTarget = 0;
+}
+
+int get_op(int instruction){
+	return (instruction >> 26) & 63;
+}
+
+
